7dc939e6b33a14b62a81be55c66f2ffcadc5df42
Cancelkam <Cancel.kam@gmail.com>
2020-02-26 00:30:54 +0300

    count miles added v1.0

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int ticketPrice = 3500;
+       int step = 20;
+       int miles = ticketPrice / step ;
+        System.out.println(miles);
+    }
+}
